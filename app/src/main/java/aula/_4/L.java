74506a500c51d09a74b20c8ed2442bad4b9f628c
Audioteca Ancap <danielpotoco@gmail.com>
2023-01-31 13:00:54 -0300

    mais exercícios

---
+++ app/src/main/java/aula/_4/L.java
@@ -0,0 +1,9 @@
+package aula._4;
+
+public class L {
+    public static void main(String[] args) {
+        for(int counter = 1; counter <= 10; counter++) {
+            System.out.println(counter);
+        }
+    }
+}
